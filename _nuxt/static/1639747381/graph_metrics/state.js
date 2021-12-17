1db03670b9fda2ad9dd24fbba72c1c0651303034
ardasener <ardasener@users.noreply.github.com>
2021-12-17 17:41:26 +0000

    deploy: 9cc511ea98e2d8b1d13c7292bd189d3a7dfcf8e7

--- _nuxt/static/1639747381/graph_metrics/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fsparsebase-frontend\u002F_nuxt\u002Fstatic\u002F1639747381",layout:"default",error:a,serverRendered:true,routePath:"\u002Fgraph_metrics",config:{_app:{basePath:"\u002Fsparsebase-frontend\u002F",assetsPath:"\u002Fsparsebase-frontend\u002F_nuxt\u002F",cdnURL:a}}}}(null));
