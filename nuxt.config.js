eea7598e7b2a090b40fc104673bc1ea0179c1b7c
ardasener <ardasenercs@gmail.com>
2021-12-17 16:21:41 +0300

    Changed router base

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,9 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
+  router: {
+    base: '/sparsebase-frontend/'
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - metrics-table-nuxt',
